9338ed0f7158b77ce3aab4c81cd456839c413b12
joshua Korf <joshkorf08@gmail.com>
2021-04-21 15:20:01 -0400

    finished API.js

---
+++ employeedirectory/src/utils/API.js
@@ -0,0 +1,13 @@
+
+import axios from 'axios';
+
+const URL = `'https://randomuser.me/api/?results=20&nat=us'`;
+
+export default {
+        
+    search: function () {
+    
+    return axios.get(URL);
+    
+    },
+};
